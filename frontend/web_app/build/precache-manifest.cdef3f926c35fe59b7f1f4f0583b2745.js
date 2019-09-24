e467d76a2b1e8c0693ed9a3d0aac14eb3298f4c5
leo <leo.bravo.rain@gmail.com>
2019-09-24 10:29:31 -0300

    deploying with firebase

---
+++ frontend/web_app/build/precache-manifest.cdef3f926c35fe59b7f1f4f0583b2745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af8e95b1753cd185f4c6b9bf8c8af29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01ec921fb21eb86a667",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a01ec921fb21eb86a667",
+    "url": "/static/js/2.638871f0.chunk.js"
+  },
+  {
+    "revision": "c4bcaf049b6fa7ba99d6",
+    "url": "/static/js/main.4080ffa1.chunk.js"
+  },
+  {
+    "revision": "ae19f639c0c7cc8fbb96",
+    "url": "/static/js/runtime~main.a85978de.js"
+  }
+]);
